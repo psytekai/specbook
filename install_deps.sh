2a6cb6fd35b5eeaec729344f6fd2a5f46f766212
ohryan <ryanachacon@gmail.com>
2025-06-25 23:41:18 -0700

    initial product scrape

---
+++ install_deps.sh
@@ -0,0 +1,13 @@
+python3 -m pip install jupyterlab
+python3 -m pip install beautifulsoup4
+
+python3 -m pip install langchain
+python3 -m pip install langchain-openai
+python3 -m pip install langgraph
+python3 -m pip install python-dotenv
+python3 -m pip install langchain-community
+python3 -m pip install pypdf
+python3 -m pip install langchain-anthropic
+python3 -m pip install tavily-python
+python3 -m pip install langgraph-checkpoint-sqlite
+python3 -m pip install langchain-tavily
