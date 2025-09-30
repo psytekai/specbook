4e371f30b7c36bd211e39bcc71e917ee0d683f2c
Ryan <ryanachacon@gmail.com>
2025-09-29 22:21:08 -0600

    update gitworkflow and remove secret key

--- electron-app/scripts/test-windows-bridge.ps1
+++ electron-app/scripts/test-windows-bridge.ps1
@@ -3,8 +3,8 @@ Remove-Item stdout.txt, stderr.txt -ErrorAction SilentlyContinue
 
 # Run with separated outputs
 $env:SPEC_BRIDGE_PAYLOAD = '{"url": "https://www.homedepot.com/p/DEWALT-20V-MAX-Cordless-Drill-Driver-Kit-DCD771C2/204279858", "options": {"method": "requests"}}'
-$env:OPENAI_API_KEY = "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"
-$env:FIRECRAWL_API_KEY = "fc-de6666a8b66346bab82f30de8b6f9abds"
+$env:OPENAI_API_KEY = ""
+$env:FIRECRAWL_API_KEY = ""
 
 $process = Start-Process -FilePath ".\electron_bridge.exe" `
     -RedirectStandardOutput "stdout.txt" `
